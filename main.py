a068b610bbbcceda4b199776ee501aed0fbed4ad
Zilliqman <zilliqman@gmail.com>
2023-03-23 16:36:42 +0100

    V1 ready to deploy

---
+++ main.py
@@ -0,0 +1,166 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('!'), description='poil', intents=intents)
+
+# import config from json file
+import json
+with open("config.json", "r") as f:
+    config = json.load(f)
+
+# import json database to read and write
+import json
+with open("database.json", "r") as f:
+    database = json.load(f)
+
+#================================
+
+# add project to database
+async def create_project(channel, name, ctx):
+    if database.get(channel) is not None:
+         await ctx.send("ERREUR : Un projet existe déjà sur ce channel !")
+         return
+    database[channel] = {"name": name, "active": "", "tasks": []}
+    save_database()
+    await ctx.send("Le projet " + name + " a été créé !")
+
+# delete project from database
+async def delete_project(channel, ctx):
+    if database.get(channel) is None:
+         await ctx.send("ERREUR : Pas de projet sur ce channel !")
+         return
+    database[channel].pop()
+    save_database()
+    await ctx.send("Le projet a été supprimé !")
+
+# see project tasks
+async def see_project(channel, ctx):
+    if database.get(channel) is None:
+         await ctx.send("ERREUR : Pas de projet sur ce channel !")
+         return
+    
+    lines = []
+    lines.append("======= " + database[channel]["name"] + " =======")
+    if database[channel]["tasks"] == []:
+        lines.append("Aucune tâche en cours...")
+        message = '\n'.join(lines)
+        await ctx.send(message)
+        return
+    for index, task in enumerate(database[channel]["tasks"]):
+        # get channel used by context
+        channel = ctx.channel
+
+        # find user corresponding to responsible in the channnel
+        currentResponsible = None
+        for member in ctx.channel.members:
+            if member.name == task["responsible"]:
+                currentResponsible = member
+        lines.append("[" + str(index + 1) + "] " + task["description"] + " - " + (currentResponsible.mention if currentResponsible is not None else task["responsible"]) + " - " + task["status"])
+    message = '\n'.join(lines)
+    await ctx.send(message)
+
+#================================
+
+# create task
+def create_task(channel, task, creator):
+    database[channel]["tasks"].append({"description": task, "status": "NEW", "responsible": creator})
+    save_database()
+
+# remove task
+def delete_task(channel, task_number):
+    task = database[channel]["tasks"][3 - 1]
+    database[channel]["tasks"].remove(task)
+    save_database()
+
+# assign responsable to task
+def assign_responsible(channel, task, responsible, ctx):
+    database[channel]["tasks"][int(task) - 1]["responsible"] = responsible
+    save_database()
+
+# finish task
+def finish_task(channel, task):
+    database[channel]["tasks"][int(task) - 1]["status"] = "DONE"
+    save_database()
+
+# cancel task
+def cancel_task(channel, task):
+    database[channel]["tasks"][int(task) - 1]["status"] = "CANCELLED"
+    save_database()
+
+#================================
+
+# save database to json file
+def save_database():
+    with open("database.json", "w") as f:
+        json.dump(database, f)
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Bonjour !")
+
+# ========== project commands ==========
+# help command
+@bot.command()
+async def phelp(ctx):
+    await ctx.send("Liste des commandes : \n!cp <nom> : créer un projet \n!dp : supprimer le projet \n!sp : voir les tâches du projet \n!ct <tâche> : créer une tâche \n!dt <numéro> : supprimer une tâche \n!rt <numéro> <responsable> : assigner un responsable à une tâche \n!ft <numéro> : terminer une tâche \n!cat <numéro> : annuler une tâche")
+
+# create new project
+@bot.command()
+async def cp(ctx, name):
+    await ctx.send("Création du projet " + name + "...")
+    channel = ctx.channel.name
+    await create_project(channel, name, ctx)
+
+# delete project
+@bot.command()
+async def dp(ctx, task):
+    channel = ctx.channel.name
+    await delete_project(channel, task)
+
+# see project
+@bot.command()
+async def sp(ctx):
+    channel = ctx.channel.name
+    await see_project(channel, ctx)
+
+# ========== tasks commands ==========
+# create task and store user data to mention him
+@bot.command()
+async def ct(ctx, *args):
+    channel = ctx.channel.name
+    creator = ctx.author.name
+    description = ' '.join(args)
+    create_task(channel, description, creator)
+
+# delete task
+@bot.command()
+async def dt(ctx, task_number):
+    channel = ctx.channel.name
+    delete_task(channel, task_number)
+
+# assign responsible to task
+@bot.command()
+async def rt(ctx, task_number, responsible):
+    channel = ctx.channel.name
+    assign_responsible(channel, task_number, responsible, ctx)
+
+# finish task
+@bot.command()
+async def ft(ctx, task_number):
+    channel = ctx.channel.name
+    finish_task(channel, task_number)
+
+# cancel task
+@bot.command()
+async def cat(ctx, task_number):
+    channel = ctx.channel.name
+    cancel_task(channel, task_number)
+
+# ========== END ==========
+
+# run bot using token on config.json
+bot.run(config["token"])
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Project management bot
+
+Permit to define projects, create tasks and assign them to people in the channel.
